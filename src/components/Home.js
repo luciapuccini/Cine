43d4ae7fb46b91efed6bfe24d580a1aa50d0acc2
Lucia <puccinilucia@gmail.com>
2019-10-14 18:54:07 -0300

    Linter change

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,29 +4,29 @@ import { Header, Button, Container } from 'semantic-ui-react';
 
 export default class Home extends Component {
   state = {
-   users : []
+    users: []
   }
 
   componentDidMount() {
-     fetch('http://localhost:8080/user/all')
-      .then(response => response.json() )
-      .then(users => this.setState({users}, ()=> console.log('set state user',this.state)))
+    fetch('http://localhost:8080/user/all')
+      .then(response => response.json())
+      .then(users => this.setState({ users }, () => console.log('set state user', this.state)))
   }
 
   render() {
     const users = this.state.users
     console.log(this.state.users);
-  
+
     return (
       <Container>
         <Header>Welcome</Header>
-        <div style={{margin:'10px'}}>
-        { users.length !== 0 ?
-          users[0].email
-          : ' '
-         }
+        <div style={{ margin: '10px' }}>
+          {users.length !== 0 ?
+            users[0].email
+            : ' '
+          }
         </div>
-  
+
         <Link to="/seats">pick a seat</Link>
       </Container>
     );
